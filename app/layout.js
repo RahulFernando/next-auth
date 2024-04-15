8c8be58f8d629c11cc525116c375a7841c6ec10d
RahulFernando <rahulrulz680@gmail.com>
2024-04-15 11:02:59 +0530

    role based authorization

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
-import { Inter } from "next/font/google";
+import AuthProvider from "./components/auth-provider";
+import Nav from "./components/nav";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +10,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="bg-gray-100">
+        <AuthProvider>
+          <Nav />
+          <div className="m-2">{children}</div>
+        </AuthProvider>
+      </body>
     </html>
   );
 }
